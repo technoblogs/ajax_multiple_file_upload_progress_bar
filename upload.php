c931176a885cb372e242484b450cb0e99082d633
Prabhat <prabhatbhardwaj111@gmail.com>
2018-04-25 23:31:10 +0530

    my first commit

---
+++ upload.php
@@ -0,0 +1,9 @@
+<?php
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["filesToBeUpload"]["name"]);
+if (move_uploaded_file($_FILES["filesToBeUpload"]["tmp_name"], $target_file)) {
+    echo "The file ". basename( $_FILES["filesToBeUpload"]["name"]). " has been uploaded.";
+} else {
+    echo "Sorry, there was an error uploading your file.";
+}
+?> 
